081ba76d40d491126a42b53ee9d767a94bf33eef
joshva7 <joshvajames163@gmai.com>
2026-06-09 02:42:19 +0530

    Employee Management System V1

---
+++ src/test/java/com/example/emplyee_manager_sysetm/EmplyeeManagerSysetmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emplyee_manager_sysetm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplyeeManagerSysetmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
